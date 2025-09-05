edf7a45b64f2803cca56741cb99d7bf6d2383ae0
mercibajgithub <146059881+mercibajgithub@users.noreply.github.com>
2025-09-06 01:17:00 +0530

    small change

--- src/assets/Watchlist.jsx
+++ src/assets/Watchlist.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Moviecard from "../component/Moviecard";
+
+import Moviecard from "../../components/MovieCard.jsx";
+
 
 const Watchlist = () => {
   const [watchlist, setWatchlist] = useState(() => {
